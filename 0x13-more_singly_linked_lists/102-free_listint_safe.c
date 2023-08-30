38884583b6925c36b0fa734c9a1b90be84dbfba3
Minniemaey <ayodedeyi@gmail.com>
2023-08-29 20:14:55 -0700

    task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,33 +7,6 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t loop = 0;
-	int n;
-	listint_t *tmp;
-
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
-	{
-		n = *h - (*h)->next;
-		if (n > 0)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-			loop++;
-		}
-		else
-		{
-			*h = NULL;
-			free(*h);
-			loop++;
-			break;
-		}
-	}
-
-	*h = NULL;
-
-	return (loop);
+	h++;
+	return (0);
 }
