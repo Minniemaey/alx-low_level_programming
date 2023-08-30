1de639e8cad21080ded59ac5aaa782151950f822
Minniemaey <ayodedeyi@gmail.com>
2023-08-29 19:42:56 -0700

    task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,24 +6,39 @@
  *
  * Return: number of nodes
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t n = 0;
-	long int node;
+	size_t a = 0, b = 0;
+	const listint_t *node, *h;
 
+	h = head;
+	node = head;
 	while (head)
 	{
-		node = head - head->next;
-		n++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (node > 0)
-			head = head->next;
-		else
+		printf("[%p] %d\n", (void *) head, head->n);
+		if (head == head->next)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *) head, head->n);
+			a++;
 			break;
 		}
+		head = head->next;
+		while (b < a && node)
+		{
+			if (head == node)
+			{
+				printf("-> [%p] %d\n", (void *) head, head->n);
+				if (head != head->next)
+					a++;
+				return (a);
+			}
+			node = node->next;
+			b++;
+		}
+		node = h;
+		b = 0;
+		a++;
 	}
-
-	return (n);
+	return (a);
 }
