367aca7fd5014e78d3b25f8fc934623f469be2ba
Minniemaey <ayodedeyi@gmail.com>
2023-08-31 13:36:07 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,20 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x = 0, y = 0;
+	int x = 0;
+	unsigned int y = 0;
 
 	if (b == NULL)
 		return (0);
 
 	while (b[x])
 	{
-		if (!(b[x] < '0' || b[x] > '1'))
-		{
-			y = 2 * y + (b[x] - '0');
-			x++;
-		}
-		else
+		if (!(b[x] == '0' || b[x] == '1'))
 			return (0);
+		y <<= 1;
+		if (b[x] == '1')
+			y += 1;
+		x++;
 	}
-	return (x);
+	return (y);
 }
