6af7bd0ebef79d198a55badd96be7cfbf86ca4e8
Minniemaey <ayodedeyi@gmail.com>
2023-10-16 18:48:23 -0700

    task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - addition of two integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction of integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of two integers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
