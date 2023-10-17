7bbe8f2b7f22313cb9d1cf9760dc646e1ef39a27
Minniemaey <ayodedeyi@gmail.com>
2023-10-16 20:12:45 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Minniemaey/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libjackpot.so
-export LD_PRELOAD =/tmp/libjackpot.so
+wget -P .. https://github.com/Minniemaey/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libjackpot.so
+export LD_PRELOAD="$PWD/../libjackpot.so"
